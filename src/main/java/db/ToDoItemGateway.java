7b272bc2110efb3fc3f2d4480dc36fec865e7754
Anton Rouba <anton.rouba@gmail.com>
2018-07-17 10:48:26 -0700

    Added ToDoItemGateway.insert() method

--- src/main/java/db/ToDoItemGateway.java
+++ src/main/java/db/ToDoItemGateway.java
@@ -1,10 +1,12 @@
 package db;
 
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Date;
 
-public class ToDoItemGateway
-{
+public class ToDoItemGateway {
+    //TODO: improve catching of exceptions
+    //TODO: add logging
+
     public void findAll() {
         String sql = "select * from to_do_item;";
 
@@ -19,7 +21,27 @@ public class ToDoItemGateway
             statement.close();
 
         } catch (Exception e) {
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public void insert(String description, String notes, Date alarmDate) {
+        String sql = "insert into to_do_item(description, notes, created_dt, updated_dt, alarm_dt, is_done) values(?, ?, ?, ?, ?, ?);";
+
+        try {
+            PreparedStatement statement = PostgresqlConnector.getConnection().prepareStatement(sql);
+            statement.setString(1, description);
+            statement.setString(2, notes);
+            statement.setTimestamp(3, new Timestamp(new Date().getTime()));
+            statement.setTimestamp(4, new Timestamp(new Date().getTime()));
+            statement.setTimestamp(5, new Timestamp(alarmDate.getTime()));
+            statement.setBoolean(6, Boolean.FALSE);
+
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
